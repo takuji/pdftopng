409dd3008d40ba8e38457aca1cb0fd3c8ab966d2
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2012-12-07 15:41:40 +0900

    Add --prefix option to change the prefix of PNG files

--- lib/pdftopng.rb
+++ lib/pdftopng.rb
@@ -4,10 +4,16 @@ require "fileutils"
 module PdfToPng
 
   # Your code goes here...
-  def self.convert(work_dir, pdf_path)
-    FileUtils.makedirs(work_dir) unless Dir.exist?(work_dir)
-    Dir.chdir(work_dir) do |path|
-      `pdftoppm #{pdf_path} page`
+  def self.convert(pdf_path, options={})
+    options = {
+      work_dir: Pathname.new("./out"),
+      prefix: nil
+    }.merge(options)
+
+    FileUtils.makedirs(options[:work_dir]) unless Dir.exist?(options[:work_dir])
+
+    Dir.chdir(options[:work_dir]) do |path|
+      `pdftoppm #{pdf_path} #{options[:prefix]}`
       Dir.entries(".").select{|e| /\.ppm$/ =~ e}.each do |ppm_path|
         png_path = to_png(ppm_path)
         `convert -resize 640x480 #{ppm_path} #{png_path}`
